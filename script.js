67e7b3c06a549b046db63f48fa20848a81134744
Pujjaa-Devops <rautpooja0687@outlook.com>
2024-12-25 14:54:42 +0530

    js added

---
+++ script.js
@@ -0,0 +1,41 @@
+const openBtn = document.querySelector(".open-modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-modal-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const successMessage = document.getElementById("success-message");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+
+function openModal() {
+    modal.classList.add("show"); 
+    successMessage.classList.add("hide"); 
+    nameInput.value = ''; 
+    emailInput.value = '';
+}
+
+function closeModal() {
+    modal.classList.remove("show"); 
+}
+
+function handleSubmit() {
+    if (nameInput.value === '' || emailInput.value === '') {
+        alert("Please fill out all fields."); 
+    } else {
+      
+        successMessage.classList.remove("hide");
+        successMessage.innerText = `Thank you, ${nameInput.value}! Your email ${emailInput.value} has been submitted.`;
+        
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
+
+
+openBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+submitBtn.addEventListener("click", handleSubmit);
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
